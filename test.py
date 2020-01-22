e2687a22a84ee010d78241da442323f059950e45
beng <beng1998@yeah.net>
2020-01-22 21:36:56 +0800

    divide test and bplus

---
+++ test.py
@@ -0,0 +1,25 @@
+from bplus import MyBPlusTree
+
+tree = MyBPlusTree()
+tree.add_key(1)
+tree.add_key(6)
+tree.add_key(3)
+tree.add_key(4)
+
+tree.add_key(9)
+tree.add_key(7)
+
+
+tree.add_key(11)
+tree.add_key(12)
+tree.add_key(13)
+tree.add_key(14)
+print(tree.find_key(13))
+
+tree.add_key(15)
+tree.add_key(17)
+tree.add_key(19)
+tree.add_key(21)
+tree.add_key(23)
+tree.add_key(25)
+print(tree.find_key(23))
